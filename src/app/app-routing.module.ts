c2fc42d57689e1ba165cf7b53782fcf02d6137d1
Daniils Firgers <dfirger@gmail.com>
2023-03-16 21:24:20 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './car/car.component';
+
+const routes: Routes = [
+  { path:'car', component: CarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
