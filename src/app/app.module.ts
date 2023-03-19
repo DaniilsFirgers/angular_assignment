8f36a1d1eebf56138a42b13d58f45bbcdf90bac1
Daniils Firgers <dfirger@gmail.com>
2023-03-19 20:37:55 +0200

    some changes made to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world.component';
 import { CarsComponent } from './cars/cars.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatListModule} from '@angular/material/list';
 import { HttpClientModule } from '@angular/common/http';
 import { CarDetailComponent } from './car-detail/car-detail.component';
-
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HelloWorldComponent, CarsComponent, CarDetailComponent, 
+    CarsComponent, CarDetailComponent, HomeComponent, 
   ],
   imports: [
     BrowserModule,
@@ -24,7 +24,12 @@ import { CarDetailComponent } from './car-detail/car-detail.component';
     BrowserAnimationsModule,
     MatMenuModule,
     MatListModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+      { path: 'cars', component: CarsComponent },
+      { path: 'home', component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
