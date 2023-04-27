6556679bfa1ff163a3365f7bda9b8ad5bba1c655
Daniils Firgers <dfirger@gmail.com>
2023-04-27 22:28:34 +0300

    add filter and validation to the app

--- src/app/car-add/car-add.component.ts
+++ src/app/car-add/car-add.component.ts
@@ -1,46 +1,54 @@
 import { Component, Input } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import { PostCarAdd} from "../../../services/post.data"
+import { FormControl } from '@angular/forms';
+import { PostCarAdd } from '../../../services/post.data';
 import { v4 as uuidv4 } from 'uuid';
-
 
 @Component({
   selector: 'app-car-add',
   templateUrl: './car-add.component.html',
-  styleUrls: ['./car-add.component.css']
+  styleUrls: ['./car-add.component.css'],
 })
 export class CarAddComponent {
-  @Input()   model = ''
-  yearProduction: number = 2020
-  price: number = 0
+  @Input() model = '';
+  yearProduction: number = 2020;
+  price: number = 0;
   selectedBrand = 'Mercedes';
-  selectedCondition = 'Used'
-  description: string = ""
+  selectedCondition = 'Used';
+  description: string = '';
+
+  formSubmitted = false;
   constructor(private addCar: PostCarAdd) {}
 
-	onBrandSelected(value:string): void {
-		this.selectedBrand = value;
-	}
-  onConditionSelected(value:string): void {
-		this.selectedCondition = value;
-	}
-  onSubmitForm(){
+  onBrandSelected(value: string): void {
+    this.selectedBrand = value;
+  }
+  onConditionSelected(value: string): void {
+    this.selectedCondition = value;
+  }
+  onSubmitForm() {
+    this.formSubmitted = true;
+
     const newCar = {
       id: uuidv4(),
       brand: this.selectedBrand,
       model: this.model,
-      "year_of_production": this.yearProduction,
-      "price": this.price,
-      "condition": this.selectedCondition,
-      "description": this.description
-    }
-    console.log(newCar)
-    this.addCar.postCar(newCar).subscribe((res)=>console.log(res))
-    this.model = ''
-    this.price =0;
+      year_of_production: this.yearProduction,
+      price: this.price,
+      condition: this.selectedCondition,
+      description: this.description,
+    };
+
+    if (newCar.brand === '' || newCar.description === '' || newCar.model === '')
+      return;
+    if (newCar.price <= 0) return;
+    if (newCar.year_of_production > 2023) return;
+
+    this.addCar.postCar(newCar).subscribe((res) => console.log(res));
+    this.model = '';
+    this.price = 0;
     this.selectedBrand = 'Mercedes';
-    this.selectedCondition = 'Used'
-    this.description = ''
-    this.yearProduction = 2020
+    this.selectedCondition = 'Used';
+    this.description = '';
+    this.yearProduction = 2020;
   }
 }
--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,41 +1,58 @@
-
-import { Component, OnInit } from '@angular/core';
-import {ActiveCarAds} from "../../../services/get.data"
-import { DeleteCarAdd} from "../../../services/delete.data"
+import { Component, Input, OnInit } from '@angular/core';
+import { ActiveCarAds } from '../../../services/get.data';
+import { DeleteCarAdd } from '../../../services/delete.data';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+  styleUrls: ['./cars.component.css'],
 })
 export class CarsComponent {
-  posts: any
+  brands: string[] = ['Mercedes', 'BMW', 'Audi', 'All'];
+  posts: any;
+  filteredPosts: any;
+  selectedBrand: string = 'All';
 
-  constructor(private carAds:ActiveCarAds, private carDeleteAdd: DeleteCarAdd) {}
+  constructor(
+    private carAds: ActiveCarAds,
+    private carDeleteAdd: DeleteCarAdd
+  ) {}
+
   ngOnInit(): void {
-      this.carAds.getPosts()
-      .subscribe(res=>{
-        if(res.status == 200){
-          console.log(res)
-          this.posts = res.body;
-        }
-        else{
-          console.log(res)
-        }
-      })
+    this.carAds.getPosts().subscribe((res) => {
+      if (res.status == 200) {
+        console.log(res);
+        this.posts = res.body;
+        this.filteredPosts = this.posts;
+      } else {
+        console.log(res);
+      }
+    });
   }
-  async onCarDeleteHandle(eventData: {car: any}){
-   this.carDeleteAdd.deleteCar(eventData.car).subscribe(res=>console.log('res', res))
-    this.carAds.getPosts()
-      .subscribe(res=>{
-        if(res.status == 200){
-          console.log(res)
-          this.posts = res.body;
-        }
-        else{
-          console.log(res)
-        }
-      })
+  async onCarDeleteHandle(eventData: { car: any }) {
+    this.carDeleteAdd
+      .deleteCar(eventData.car)
+      .subscribe((res) => console.log('res', res));
+    this.carAds.getPosts().subscribe((res) => {
+      if (res.status == 200) {
+        console.log(res);
+        this.posts = res.body;
+      } else {
+        console.log(res);
+      }
+    });
+  }
+  filterPosts(e: Event) {
+    const brand = (e.target as HTMLSelectElement).value;
+    this.selectedBrand = brand;
+    console.log(this.selectedBrand);
 
+    if (this.selectedBrand === 'All') {
+      this.filteredPosts = this.posts;
+    } else {
+      this.filteredPosts = this.posts.filter(
+        (el: any) => el.brand === this.selectedBrand
+      );
+    }
   }
 }
